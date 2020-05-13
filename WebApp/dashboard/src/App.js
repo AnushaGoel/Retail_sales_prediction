bc9a71df3d12495b56008536eec44e54aa8801ed
AnushaGoel <60611755+AnushaGoel@users.noreply.github.com>
2020-05-13 16:09:10 +0530

    Update App.js

--- WebApp/dashboard/src/App.js
+++ WebApp/dashboard/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import ProductDetails from './ProductDetails';
 
 function App() {
   return (
     <div className="App">
-      React Boiler Plate
+      <ProductDetails />
     </div>
   );
 }
